ec09d083fe22ad1f048c750b9b165dbee7c08ed5
Eric Dattore <edattore@gmail.com>
2016-06-26 00:09:42 -0600

    Add frontend

---
+++ voting-client/src/components/Voting.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Winner from './Winner';
+import Vote from './Vote';
+
+export default class Voting extends React.Component {
+  getPair() {
+    return this.props.pair || [];
+  }
+
+  isDisabled() {
+    return !!this.props.hasVoted;
+  }
+
+  hasVotedFor(entry) {
+    return this.props.hasVoted === entry;
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.winner ?
+          <Winner ref="winner" winner={this.props.winner} />:
+          <Vote {...this.props} />}
+      </div>
+    );
+  }
+}
